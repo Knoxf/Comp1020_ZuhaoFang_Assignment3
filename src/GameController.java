2e52390bb6dc441e5a5c4ec22080b7ff2d52dc96
ZuhaoFang <knoxfang@outlook.com>
2020-03-14 00:07:46 -0500

    Finnish phase 3a

--- src/GameController.java
+++ src/GameController.java
@@ -1,33 +1,37 @@
+import java.util.ArrayList;
+
 public class GameController {
 
     public static void main(String[] args){
-        draw1();
-        draw2();
-    }
+        // Created ArrayList
+        ArrayList<Thing> allThings = new ArrayList<>();
 
-
-    public static void draw1(){
+        // created first Thing object
         Thing draw1 = new Thing("draw1","nothing");
         draw1.isDrawn = true;
         draw1.drawFile = "groundRock.png";
         draw1.position.x = .5;
         draw1.position.y = .5;
-        while(draw1.isDrawn){
-            draw1.draw();
-            StdDraw.show(60);
-        }
-    }
 
-    public static void draw2(){
+        // created second Thing object
         Thing draw = new Thing("Draw1", "nothing");
         draw.isDrawn = true;
-        draw.drawFile = "Person0.png";
+        draw.drawFile = "ground.png";
         draw.position.x = 0.3;
         draw.position.y = .2;
 
-        while(draw.isDrawn){
-            draw.draw();
-            StdDraw.show(60);
+        // Add Those two think into the ArrayList
+        allThings.add(draw1);
+        allThings.add(draw);
+
+        for(int i=0;i<allThings.size();i++){
+            while (allThings.get(i).isDrawn){
+                allThings.get(i).update();
+                allThings.get(i).draw();
+                StdDraw.show(2);
+            }
         }
+
     }
+
 }
